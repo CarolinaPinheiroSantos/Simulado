1637a561d6cf5ad80b676accbac5d43c6affe65a
CarolinaPinheiroSantos <pinheirocarol0506@gmail.com>
2025-08-27 11:17:39 -0300

    27/08/25

--- back/kanban/appKanban/models.py
+++ back/kanban/appKanban/models.py
@@ -8,8 +8,19 @@ class Usuarios(models.Model):
     def __str__(self):
         return self.nome
     
-class Tarefa():
+class Tarefa(models.Model):
+    
+    choices_prioridade = [("baixa", "baixa"), ("media", "media"), ("alta", "alta")]
+    
+    choices_status = [("feito", "feito"), ("fazendo", "fazendo"), ("aFazer", "a fazer")]
+    
     idTarefa = models.IntegerField()
-    descricao = models.CharField()
-    setor  = models.
+    descricao = models.TextField()
+    setor  = models.CharField(max_length=50)
+    prioridade = models.CharField(max_length=10, choices = choices_prioridade)
+    data = models.DateField()
+    status = models.CharField(max_length=10, choices= choices_status)
+    usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.descricao
