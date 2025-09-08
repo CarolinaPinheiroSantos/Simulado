05ea011648e9b1472aa7af1de943d252aaee36dc
Carolina Pinheiro dos Santos <54641272832@>
2025-09-08 14:06:30 -0300

    08/09/25

--- back/kanban/kanban/urls.py
+++ back/kanban/kanban/urls.py
@@ -17,25 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-# from django.contrib.auth.models import User
-# from rest_framework import routers, serializers, viewsets
-
-# # Serializers define the API representation.
-# class UserSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ['url', 'username', 'email', 'is_staff']
-
-# # ViewSets define the view behavior.
-# class UserViewSet(viewsets.ModelViewSet):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-
-# router = routers.DefaultRouter()
-# router.register(UserViewSet)
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include('appKanban.urls'))
 ]
